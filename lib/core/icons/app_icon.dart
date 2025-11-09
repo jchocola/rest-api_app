73c4c8524be15d3cddfd1ad166d2369c1a3edb70
Bak Nguen <sangsangden@gmail.com>
2025-11-09 15:23:48 +0700

    drawer info

--- lib/core/icons/app_icon.dart
+++ lib/core/icons/app_icon.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 
-
 class AppIcons {
   static const IconData historyIcon = Iconsax.book;
   static const IconData menuIcon = Iconsax.menu;
+
+  // drawer icons
+  static const IconData rateUsIcon = Iconsax.star;
+  static const IconData shareIcon = Iconsax.share4;
+  static const IconData privacyIcon = Iconsax.lock;
+  static const IconData feedbackIcon = Icons.feedback_outlined;
+  static const IconData aboutUsIcon = Iconsax.info_circle;
 }
--- lib/presentation/drawer_page/drawer_page.dart
+++ lib/presentation/drawer_page/drawer_page.dart
@@ -1,4 +1,5 @@
 import 'package:api_client/core/constant/app_constant.dart';
+import 'package:api_client/core/icons/app_icon.dart';
 import 'package:flutter/material.dart';
 import 'package:shadcn_ui/shadcn_ui.dart';
 
@@ -32,38 +33,38 @@ class DrawerPage extends StatelessWidget {
 
             spacing: 16,
             children: [
-              ShadButton.ghost(
-                leading: Icon(Icons.language),
-                child: Text('Language'),
+              // ShadButton.ghost(
+              //   leading: Icon(Icons.language),
+              //   child: Text('Language'),
+              // ),
+
+               ShadButton.ghost(
+                leading: Icon(AppIcons.rateUsIcon),
+                child: Text('Rate us'),
               ),
 
                ShadButton.ghost(
-                leading: Icon(Icons.language),
-                child: Text('Rate us on Play Store'),
-              ),
-
-               ShadButton.ghost(
-                leading: Icon(Icons.language),
+                leading: Icon(AppIcons.shareIcon),
                 child: Text('Share with a friend'),
               ),
 
                ShadButton.ghost(
-                leading: Icon(Icons.language),
+                leading: Icon(AppIcons.privacyIcon),
                 child: Text('Privacy Policy'),
               ),
 
-               ShadButton.ghost(
-                leading: Icon(Icons.language),
-                child: Text('Terms of Service'),
-              ),
+              //  ShadButton.ghost(
+              //   leading: Icon(Icons.language),
+              //   child: Text('Terms of Service'),
+              // ),
 
                ShadButton.ghost(
-                leading: Icon(Icons.language),
+                leading: Icon(AppIcons.feedbackIcon),
                 child: Text('Feedback'),
               ),
 
                ShadButton.ghost(
-                leading: Icon(Icons.language),
+                leading: Icon(AppIcons.aboutUsIcon),
                 child: Text('About Us'),
               ),
             ],
